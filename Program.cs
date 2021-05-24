3acd502aafebd1862b239503623893bba1b44c31
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-05-24 13:47:21 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+           
+            Stack<char> openParenthesis = new Stack<char>();
+            bool isBalanced = true;
+
+            foreach (char c in expression)    // {[()]}
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    openParenthesis.Push(c);
+                }
+                else
+                {  
+                    if (!openParenthesis.Any())
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+
+                    char currOpenParenthesis = openParenthesis.Pop();
+
+                    bool isRoundBalanced = currOpenParenthesis == '(' && c == ')';
+                    bool isCurlyBalanced = currOpenParenthesis == '{' && c == '}';
+                    bool isSquareBalanced = currOpenParenthesis == '[' && c == ']';
+
+                    if (isRoundBalanced == false && 
+                        isCurlyBalanced == false && isSquareBalanced == false)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                } 
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        //}
+    }
+}
